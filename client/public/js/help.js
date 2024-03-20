c03c8e4147c74088fe710ab55ec8c79cd137a9ad
Harsh Singh <harsh_singh@srmap.edu.in>
2024-03-20 18:41:27 +0530

    help_page

--- client/public/js/help.js
+++ client/public/js/help.js
@@ -5,7 +5,7 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = "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"; // Paste your API key here
+const API_KEY = " API is here"; // Paste your API key here
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
